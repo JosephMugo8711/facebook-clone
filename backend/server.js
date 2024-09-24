df0a840de1fbb3633b1bbcf88375575f6b22a79c
JosephMugo8711 <wamiti8711@gmail.com>
2024-09-24 15:22:36 +0300

    Feat: testing cors

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-const allowed =  ["http://localhost:5173", "some other link"];
-
-function options(req, res){
-    let tmp;
-    let origin = req.header("Origin");
-    if(allowed.indexOf(origin) > -1){
-        tmp = {
-            origin: true,
-            optionSuccessStatus: 200,
-        };
-    } else {
-        tmp = {
-            origin: "wait"
-        };
-    }
-    res(null, tmp);
-}
-
-
-app.use(cors(options));
-
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Welcome");
 });
